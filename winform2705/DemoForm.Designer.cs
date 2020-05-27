249a2ffb160de0f3c691304d8888951156fdde14
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-05-27 21:23:42 +0300

    Add files via upload

---
+++ winform2705/DemoForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace winform2705
+{
+    partial class DemoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numLbl = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.myTimer = new System.Windows.Forms.Timer(this.components);
+            this.startStopTimerBtn = new System.Windows.Forms.Button();
+            this.timeTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numLbl
+            // 
+            this.numLbl.AutoSize = true;
+            this.numLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.numLbl.Location = new System.Drawing.Point(179, 81);
+            this.numLbl.Name = "numLbl";
+            this.numLbl.Size = new System.Drawing.Size(30, 31);
+            this.numLbl.TabIndex = 0;
+            this.numLbl.Text = "0";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(99, 151);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(175, 34);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start using task run";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // myTimer
+            // 
+            this.myTimer.Tick += new System.EventHandler(this.myTimer_Tick);
+            // 
+            // startStopTimerBtn
+            // 
+            this.startStopTimerBtn.Location = new System.Drawing.Point(99, 215);
+            this.startStopTimerBtn.Name = "startStopTimerBtn";
+            this.startStopTimerBtn.Size = new System.Drawing.Size(175, 36);
+            this.startStopTimerBtn.TabIndex = 2;
+            this.startStopTimerBtn.Text = "Start using Timer";
+            this.startStopTimerBtn.UseVisualStyleBackColor = true;
+            this.startStopTimerBtn.Click += new System.EventHandler(this.startStopTimerBtn_Click);
+            // 
+            // timeTimer
+            // 
+            this.timeTimer.Interval = 10;
+            this.timeTimer.Tick += new System.EventHandler(this.timeTimer_Tick);
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.AutoSize = true;
+            this.timeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.timeLbl.Location = new System.Drawing.Point(108, 32);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(30, 31);
+            this.timeLbl.TabIndex = 3;
+            this.timeLbl.Text = "0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::winform2705.Properties.Resources.aspnet;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 191);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 132);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DemoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 324);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.timeLbl);
+            this.Controls.Add(this.startStopTimerBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.numLbl);
+            this.Name = "DemoForm";
+            this.Text = "Demo";
+            this.Load += new System.EventHandler(this.DemoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numLbl;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Timer myTimer;
+        private System.Windows.Forms.Button startStopTimerBtn;
+        private System.Windows.Forms.Timer timeTimer;
+        private System.Windows.Forms.Label timeLbl;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ winform2705/DemoForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform2705
+{
+    public partial class DemoForm : Form
+    {
+        int counter = 1;
+
+        public DemoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            // this does NOT work   
+            /*
+            Task.Run(
+                () =>
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        Thread.Sleep(100);
+                        numLbl.Text = i.ToString();
+                    }
+                });
+                */
+
+            // 1. create a new task (worker) which is not the UI thread
+            Task.Run(
+                () =>
+                {
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        // instead-
+                        // ask the UI thread to draw the label...
+                        // ???
+
+                        // chinese
+                        // 2. define Action
+                        //    action is a set of commands 
+                        Action action = () => { numLbl.Text = i.ToString(); };
+
+                        // 3. ask for UI to perform the action on the label
+                        numLbl.BeginInvoke(action);
+
+                        Thread.Sleep(500);
+                    }
+                });
+        }
+
+        private void myTimer_Tick(object sender, EventArgs e)
+        {
+            numLbl.Text = counter.ToString();
+
+            counter++;
+
+            // the timer can stop itself
+           // if (counter >= 10)
+            //    myTimer.Enabled = false;
+        }
+
+        private void DemoForm_Load(object sender, EventArgs e)
+        {
+            //myTimer.Enabled = true;
+            //timeTimer.Enabled = true;
+
+            PictureBox picture = new PictureBox
+            {
+                Name = "pictureBox",
+                Size = new Size(300, 300),
+                Location = new Point(100, 0),
+                Image = Image.FromFile("Resources/aspnet.jpg"),
+
+            };
+            this.Controls.Add(picture);
+
+            Label label = new Label // auto initializer
+            {
+                Name = "mylabel2",
+                Text = "hello...",
+                Location = new Point(0, 0)
+            };
+            this.Controls.Add(label);
+        }
+
+        private void startStopTimerBtn_Click(object sender, EventArgs e)
+        {
+            if (myTimer.Enabled == true)
+            {
+                //myTimer.Enabled = false;
+                myTimer.Stop();
+                startStopTimerBtn.Text = "Start the Timer";
+            }
+            else
+            {
+                //myTimer.Enabled = true;
+                myTimer.Start();
+                startStopTimerBtn.Text = "Stop the Timer";
+            }
+        }
+
+        private void timeTimer_Tick(object sender, EventArgs e)
+        {
+            timeLbl.Text = DateTime.Now.TimeOfDay.ToString();
+        }
+    }
+}
